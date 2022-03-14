2d9a752ac2d05080f676d323eca6d70df25185f7
GiovanniCalia <johnca@outlook.it>
2022-03-14 22:54:47 +0100

    Creazione proprietà done

--- js/script.js
+++ js/script.js
@@ -1,31 +1,29 @@
 const app = new Vue({
     el: "#root",
     data: {
-        decoration:{
-            textDecoration: "line-through",
-        },
+        done: true,
         newTodo: "",
         testi:[
-        "Fare i compiti",
-        "Fare la spesa",
-        "Fare il bucato",
+            "Fare i compiti",
+            "Fare la spesa",
+            "Fare il bucato",
         ]
     },
     methods: {
         addText(){
             if (this.newTodo != ""){
             this.testi.push(this.newTodo);
-            this.newTodo = ""
+            this.newTodo = "";
         }
         },
         deleteText(i){
             this.testi.splice(i, 1)
         },
-        addClass(){
-            this.decoration;
-        },
         addTodo(){
-            this.addText()
+            this.addText();
+        },
+        addClass(){
+            this.done == false;  
         }
     }
 })
