bbe161e352b02a3fa091cb6838d9b3d4a0500b11
GiovanniCalia <johnca@outlook.it>
2022-03-14 16:45:28 +0100

    Creazione bottoni

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 const app = new Vue({
     el: "#root",
     data: {
+        newTodo: "",
         testi:[
         "Fare i compiti",
         "Fare la spesa",
         "Fare il bucato",
         ]
     },
+    methods: {
+        addText(){
+            this.testi.push(this.newTodo)
+        },
+        deleteText(i){
+            this.testi.splice(i, 1)
+        }
+    }
 })
